e492ec68e5cd44bb37aa8f9672fc7fb427b5c431
Jessica Sendejo <jsendejo1@mail.sfsu.edu>
2023-04-25 08:55:01 -0700

    Initial Commit

---
+++ Dessert/DessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertApp.swift
+//  Dessert
+//
+//  Created by Jessica Sendejo on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
